f6220e01e7e5870822642af610387817fc176c52
Eugenio Regis <kuque84@hotmail.com>
2022-08-31 12:49:22 -0300

    cambios a router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,24 @@
+/* ORIGINAL */
 const router = require('express').Router()
 const clienteRouter = require('./Cliente.routes')
 const productoRouter = require('./Producto.routes')
 const compraRouter = require('./Compra.routes')
+/* /ORIGINAL */
 
+/* EDIT */
+const camionRouter = require('./Camion.routes')
+/* /EDIT */
+
+/* ORIGINAL */
 router.use('/clientes', clienteRouter)
 router.use('/productos', productoRouter)
 router.use('/compras', compraRouter)
+/* /ORIGINAL */
 
+/* EDIT */
+
+/* /EDIT */
+
+/* ORIGINAL */
 module.exports = router;
+/* /ORIGINAL */
