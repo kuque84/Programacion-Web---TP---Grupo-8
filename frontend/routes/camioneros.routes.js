2899661b5c47fbed9cc41a6c4676d582e5d6b2dd
Eugenio Regis <kuque84@hotmail.com>
2022-09-01 20:49:59 -0300

    camioneros htmls

---
+++ frontend/routes/camioneros.routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const path = require('path');
+
+// Listar camioneros --> /camioneros
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve('./views/camioneros/listarCamioneros.html'));
+})
+
+// Crear camioneros --> /camioneros/create
+router.get('/create', (req, res) => {
+    res.sendFile(path.resolve('./views/camioneros/crearCamionero.html'));
+})
+
+// Editar camioneros --> /camioneros/update/:id
+router.get('/update/:id', (req, res) => {
+    res.sendFile(path.resolve('./views/camioneros/editarCamionero.html'));
+})
+
+module.exports = router;
--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,14 +1,38 @@
+/*  ORIGINAL    */
 const router = require('express').Router();
 const path = require('path');
+/*  /ORIGINAL    */
 
+/*      EDIT        */
+/*      /EDIT        */
+
+/*  ORIGINAL    */
 const clientesRouter = require('./clientes.routes')
 const productosRouter = require('./productos.routes')
+/*  /ORIGINAL    */
 
+/*      EDIT        */
+const camionerosRouter = require('./camioneros.routes')
+/*      /EDIT        */
+
+/*  ORIGINAL    */
 router.get('/', (req, res) => {
     res.sendFile(path.resolve('./views/index.html'));
 })
+/*  /ORIGINAL    */
 
+/*      EDIT        */
+
+/*      /EDIT        */
+
+/*  ORIGINAL    */
 router.use('/clients', clientesRouter)
 router.use('/products', productosRouter)
+/*  /ORIGINAL    */
+/*      EDIT        */
+router.use('/camioneros',camionerosRouter)
+/*      /EDIT        */
 
+/*  ORIGINAL    */
 module.exports = router;
+/*  /ORIGINAL    */
