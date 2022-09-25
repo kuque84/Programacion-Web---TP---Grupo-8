4baac331fb835b21f6d61474360d0f8c155984cc
Eugenio Regis <kuque84@hotmail.com>
2022-09-25 14:36:27 -0300

    correcion a html y js

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 
 const camionerosRouter = require('./camioneros.routes')
-const camoinesRouter = require('./camiones.routes')
+const camionesRouter = require('./camiones.routes')
 
 router.get('/', (req, res) => {
     res.sendFile(path.resolve('./views/index.html'));
